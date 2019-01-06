0d7d9661ffc492b0ef5ed25e9450182cdd4ab8b3
wanyinsheng <wan_yinsheng@163.com>
2019-01-06 22:43:41 +0800

    commit MissingNumber.java

--- src/main/java/com/wanali/array/MissingNumber.java
+++ src/main/java/com/wanali/array/MissingNumber.java
@@ -1,23 +1,23 @@
 package com.wanali.array;
+
 /**
- * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
- * 输入: [3,0,1]
- * 输出: 2
- * 输入: [9,6,4,2,3,5,7,0,1]
- * 输出: 8
+ * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。 输入: [3,0,1] 输出: 2
+ * 输入: [9,6,4,2,3,5,7,0,1] 输出: 8
+ * 
  * @author wan_ys
  *
- * 2019年1月6日下午10:27:51
+ *         2019年1月6日下午10:27:51
  */
 public class MissingNumber {
 	public static void main(String[] args) {
 		int[] nums = new int[] { 3, 0, 1 };
 		System.out.println(missingNumber(nums));
-		int[] num = new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+		int[] num = new int[] { 8, 6, 4, 2, 3, 5, 7, 0, 1 };
 		System.out.println(missingNumber(num));
 	}
 
 	static int missingNumber(int[] nums) {
+		int n = nums.length;
 		int sum = 0;
 		int max = nums[0];
 		int missNum = 0;
@@ -27,8 +27,14 @@ public class MissingNumber {
 				max = nums[i];
 			}
 		}
-		// 根据前n个数字之和与该缺少元素数组比较确定缺失数字
-		missNum = max * (max + 1) / 2 - sum;
+		// 判断是否缺失数字为最大的一个
+		if (n != max) {
+			missNum=n;
+		} else {
+			// 根据前n个数字之和与该缺少元素数组比较确定缺失数字
+			missNum = max * (max + 1) / 2 - sum;
+		}
+
 		return missNum;
 	}
 
