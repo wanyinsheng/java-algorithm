03618e72af331383e0ed74a03d28cadd6f3d9f40
wanyinsheng <wan_yinsheng@163.com>
2019-01-24 22:30:04 +0800

    commit 平方数之和

--- src/main/java/com/wanali/math/JudgeSquareSum.java
+++ src/main/java/com/wanali/math/JudgeSquareSum.java
@@ -1,11 +1,9 @@
 package com.wanali.math;
 
 /**
- * 平方数之和 
+ * 平方数之和
  * 给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a^2 + b^2 = c
- * 
  * @author wan_ys
- *
  * 2019年1月24日下午7:54:18
  */
 public class JudgeSquareSum {
