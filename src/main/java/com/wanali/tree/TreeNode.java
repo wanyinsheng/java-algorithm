d30f45b66628026ac6698d44d2bb2da5d4168513
wanyinsheng <wan_yinsheng@163.com>
2019-02-06 18:49:13 +0800

    commit 树的节点

---
+++ src/main/java/com/wanali/tree/TreeNode.java
@@ -0,0 +1,15 @@
+package com.wanali.tree;
+/**
+ * 树的节点
+ * @author wan_ys
+ *
+ * 2019年2月6日下午6:48:29
+ */
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x) {
+		val = x;
+	}
+}
