c74b4c8979ceaf62b3a8a4e4e35252d71afe1c9d
wanyinsheng <wan_yinsheng@163.com>
2019-02-19 19:36:16 +0800

    commit 查询后的偶数和

---
+++ src/main/java/com/wanali/array/SumEvenAfterQueries.java
@@ -0,0 +1,56 @@
+package com.wanali.array;
+
+import java.util.Arrays;
+
+/**
+ * 查询后的偶数和
+ * 
+ * 给出一个整数数组 A 和一个查询数组 queries。
+ * 
+ * 对于第 i 次查询，有 val = queries[i][0], index = queries[i][1]，我们会把 val 加到 A[index]
+ * 上。然后，第 i 次查询的答案是 A 中偶数值的和。
+ * 
+ * （此处给定的 index = queries[i][1] 是从 0 开始的索引，每次查询都会永久修改数组 A。）
+ * 
+ * 返回所有查询的答案。你的答案应当以数组 answer 给出，answer[i] 为第 i 次查询的答案。
+ * 
+ * 输入：A = [1,2,3,4], queries = [[1,0],[-3,1],[-4,0],[2,3]] 输出：[8,6,2,4] 
+ * 解释：
+ * 开始时，数组为 [1,2,3,4]。 将 1 加到 A[0] 上之后，数组为 [2,2,3,4]，偶数值之和为 2 + 2 + 4 = 8。 将 -3
+ * 加到 A[1] 上之后，数组为 [2,-1,3,4]，偶数值之和为 2 + 4 = 6。 将 -4 加到 A[0] 上之后，数组为
+ * [-2,-1,3,4]，偶数值之和为 -2 + 4 = 2。 将 2 加到 A[3] 上之后，数组为 [-2,-1,3,6]，偶数值之和为 -2 + 6
+ * = 4。
+ * 
+ * @author wan_ys
+ *
+ *         2019年2月19日下午7:33:06
+ */
+public class SumEvenAfterQueries {
+	public static void main(String[] args) {
+		int[] A = new int[] { 1, 2, 3, 4 };
+		int[][] queries = new int[][] { { 1, 0 }, { -3, 1 }, { -4, 0 }, { 2, 3 } };
+		System.out.println(Arrays.toString(sumEvenAfterQueries(A, queries)));
+	}
+
+	static int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+		int[] result = new int[A.length];
+		int sum = 0;// 计算每次查询之后的偶数和
+		int index = 0;
+		int val = 0;
+		for (int i = 0; i < result.length; i++) {
+			index = queries[i][1];// 求索引
+			val = queries[i][0];// 求index处应该加的值
+			A[index] = A[index] + val;// 修改A数组index处的值
+			// 求偶数和
+			for (int j = 0; j < result.length; j++) {
+				if (A[j] % 2 == 0) {
+					sum += A[j];
+				}
+			}
+			result[i] = sum;//将偶数和赋给result数组
+			sum = 0;
+		}
+		return result;
+	}
+
+}
